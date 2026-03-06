bfb9df70a30198dca32ad30e2426fcc5d5c80164
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-06 19:18:19 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,31 +1,10 @@
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     int searchInsert(std::vector<int>& nums, int target) {
-        if (nums.empty()) return 0;
-
-        if (target <= nums[0]) return 0;
-        if (target > nums.back()) return nums.size();
-
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-
-        return left;
+        return std::distance(std::begin(nums), 
+            std::lower_bound(std::begin(nums), std::end(nums), target));
     }
 };

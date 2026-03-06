73d8d1b00e2ef317612e8e06febef6e0ed798a0c
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-06 18:51:07 +0300

    Time: 0 ms (100%), Space: 22.7 MB (18.32%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        size_t k = 1;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                ++k;
+            }
+        }
+
+        return k;
+    }
+};

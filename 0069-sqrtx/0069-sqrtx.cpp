6c720d026c75f2ca9111657bcbe5723e52ff0aa7
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-08 13:34:12 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,12 +3,26 @@ public:
     int mySqrt(int x) {
         if (x <= 1) return x;
 
-        // Newton's iterative formula
-        long long r = x;
-        while (r * r > x) {
-            r = (r + x / r) / 2;
+        int left = 1;
+        int right = x / 2;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = static_cast<long long>(mid) * mid;
+
+            if (square == x) {
+                return mid;
+            }
+            else if (square < x) {
+                result = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
 
-        return r;
+        return result;
     }
 };

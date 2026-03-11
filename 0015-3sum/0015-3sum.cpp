f42b97b44c29a14f0b100d04d3db7b7fe94356d2
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-11 09:39:26 +0300

    Time: 44 ms (76.3%), Space: 29.1 MB (71.97%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        int n = nums.size();
+
+        if (n < 3) return result;
+
+        std::sort(nums.begin(), nums.end());
+
+        if (nums[0] > 0) return result;
+
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            if (nums[i] > 0) break;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({ nums[i], nums[left], nums[right] });
+
+                    int leftVal = nums[left];
+                    int rightVal = nums[right];
+
+                    while (left < right && nums[left] == leftVal) ++left;
+                    while (left < right && nums[right] == rightVal) --right;
+                }
+                else if (sum < 0) {
+                    ++left;
+                }
+                else {
+                    --right;
+                }
+            }
+        }
+
+        return result;
+    }
+};

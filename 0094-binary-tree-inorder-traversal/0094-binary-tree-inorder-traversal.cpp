b0bf91165ce8fbb4bea0299ba0acdc47440e99c4
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-12 11:50:56 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -15,16 +15,32 @@ class Solution {
 public:
     std::vector<int> inorderTraversal(TreeNode* root) {
         std::vector<int> result;
-        inorderHelper(root, result);
+        TreeNode* current = root;
+
+        while (current != nullptr) {
+            if (current->left == nullptr) {
+                result.push_back(current->val);
+                current = current->right;
+            }
+            else {
+                TreeNode* predecessor = current->left;
+
+                while (predecessor->right != nullptr && predecessor->right != current) {
+                    predecessor = predecessor->right;
+                }
+
+                if (predecessor->right == nullptr) {
+                    predecessor->right = current;
+                    current = current->left;
+                }
+                else {
+                    predecessor->right = nullptr;
+                    result.push_back(current->val);
+                    current = current->right;
+                }
+            }
+        }
+
         return result;
-    }
-
-private:
-    void inorderHelper(TreeNode* node, std::vector<int>& result) {
-        if (!node) return;
-
-        inorderHelper(node->left, result);
-        result.emplace_back(node->val);
-        inorderHelper(node->right, result);
     }
 };

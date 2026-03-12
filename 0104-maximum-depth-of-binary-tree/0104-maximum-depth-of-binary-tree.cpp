3c201c73906a3afc40a44d90eb3b711faf1c175d
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-12 12:46:01 +0300

    Time: 0 ms (100%), Space: 19 MB (77.06%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        auto maxDepthLeft  = maxDepth(root->left);
+        auto maxDepthRight = maxDepth(root->right);
+
+        return std::max(maxDepthLeft, maxDepthRight) + 1;
+    }
+};

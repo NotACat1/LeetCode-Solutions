15300a4caab25cde5135ecf7c1ebbd82365814d1
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-13 17:42:19 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -16,18 +16,14 @@ public:
     TreeNode* sortedArrayToBST(std::vector<int>& nums) {
         if (nums.empty()) return nullptr;
 
-        return buildBST(nums, 0, nums.size() - 1);
-    }
-
-private:
-    TreeNode* buildBST(std::vector<int>& nums, int left, int right) {
-        if (left > right) return nullptr;
-
-        int mid = left + (right - left) / 2;
+        int mid = nums.size() / 2;
         TreeNode* root = new TreeNode(nums[mid]);
 
-        root->left  = buildBST(nums, left, mid - 1);
-        root->right = buildBST(nums, mid + 1, right);
+        std::vector<int> left(std::begin(nums), std::begin(nums) + mid);
+        std::vector<int> right(std::begin(nums) + mid + 1, std::end(nums));
+
+        root->left  = sortedArrayToBST(left);
+        root->right = sortedArrayToBST(right);
 
         return root;
     }

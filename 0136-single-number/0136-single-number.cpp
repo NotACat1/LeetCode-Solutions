b489f7e5b346ff474447e13896f30b8de82f89dd
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-14 17:42:33 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,15 @@
 #include <vector>
-#include <numeric>
 
 class Solution {
 public:
     int singleNumber(std::vector<int>& nums) {
-        return std::reduce(std::begin(nums), std::end(nums), 0,
-            [](int a, int b) { return a ^ b; });
+        int result = 0;
+
+        for (auto num : nums)
+        {
+            result ^= num;
+        }
+
+        return result;
     }
 };

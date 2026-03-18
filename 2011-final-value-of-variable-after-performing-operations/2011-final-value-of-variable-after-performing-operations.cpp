662ae1f1510a8812ef97995a55b39ead048bdee6
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-18 14:58:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -6,8 +6,14 @@ public:
     int finalValueAfterOperations(std::vector<std::string>& operations) {
         int result = 0;
         for (auto& operation : operations) {
-            if (operation == "++X" || operation == "X++") ++result;
-            if (operation == "--X" || operation == "X--") --result;
+            if (operation[1] == '+')
+            {
+                ++result;
+            }
+            else
+            {
+                --result;
+            }
         }
         return result;
     }

8c76d7545b2d1b70caa8efa3f52efcbb3f6ce925
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-03-19 14:55:00 +0300

    Time: 0 ms (100%), Space: 9.2 MB (64.91%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,23 +4,26 @@
 class Solution {
 public:
     bool hasSameDigits(std::string s) {
-        auto n = s.length();
-        std::vector<int> row(n - 1, 1);
+        auto n = s.size();
+        std::vector<int> row(n - 1, 0);
+        row[0] = 1;
 
-        for (int len = 1; len < n - 1; ++len) {
-            for (int j = len; j > 0; --j) {
+        for (int i = 1; i <= n - 2; i++) {
+            for (int j = i; j > 0; j--) {
                 row[j] = (row[j] + row[j - 1]) % 10;
             }
         }
 
         int left = 0, right = 0;
 
-        for (int i = 0; i < n - 1; ++i) {
-            left = (left + row[i] * (s[i] - '0')) % 10;
+        for (int i = 0; i < n - 1; i++) {
+            int digit = s[i] - '0';
+            left = (left + row[i] * digit) % 10;
         }
 
-        for (int i = 0; i < n - 1; ++i) {
-            right = (right + row[i] * (s[i + 1] - '0')) % 10;
+        for (int i = 0; i < n - 1; i++) {
+            int digit = s[i + 1] - '0';
+            right = (right + row[i] * digit) % 10;
         }
 
         return left == right;

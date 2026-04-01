91971315e2580430f7c44166f4002de1f0108e76
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-04-01 12:04:53 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,23 @@
 #include <string>
-#include <algorithm>
+#include <array>
 
 class Solution {
 public:
     bool isAnagram(std::string s, std::string t) {
         if (s.size() != t.size()) return false;
 
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
+        std::array<int, 26> count = { 0 };
 
-        return s == t;
+        for (auto const ch : s)
+        {
+            ++count[ch - 'a'];
+        }
+
+        for (auto const ch : t)
+        {
+            if (--count[ch - 'a'] < 0) return false;
+        }
+
+        return true;
     }
 };

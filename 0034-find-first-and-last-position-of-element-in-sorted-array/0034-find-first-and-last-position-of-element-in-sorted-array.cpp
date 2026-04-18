1e12e9d6bbcb217b8761cd6ce7b4b03c583d90c3
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-04-18 18:58:51 +0300

    Time: 0 ms (100%), Space: 17.7 MB (18.79%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+class Solution {
+private:
+    int findBound(std::vector<int>& nums, int target, bool isFirst) {
+        int left = 0;
+        int right = static_cast<int>(nums.size()) - 1;
+        int bound = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                bound = mid;
+
+                if (isFirst) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return bound;
+    }
+
+public:
+    std::vector<int> searchRange(std::vector<int>& nums, int target) {
+        int first = findBound(nums, target, true);
+
+        if (first == -1) return { -1, -1 };
+
+        int last = findBound(nums, target, false);
+
+        return { first, last };
+    }
+};

ddc1228cfbb77359a4dfebc448cbd734bc55a7e3
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-04-19 16:18:07 +0300

    Time: 4 ms (32.32%), Space: 10.8 MB (16.51%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::string multiply(std::string num1, std::string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+
+        int n = num1.size();
+        int m = num2.size();
+        std::vector<int> res(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+
+                int sum = mul + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        std::string result = "";
+        for (int digit : res) {
+            if (!(result.empty() && digit == 0)) {
+                result += std::to_string(digit);
+            }
+        }
+
+        return result.empty() ? "0" : result;
+    }
+};

91b25156840920b005ef3acf9baf7b5d60469a44
Ryabov Daniil [NotACat] <113008873+NotACat1@users.noreply.github.com>
2026-04-20 11:09:54 +0300

    Time: 2 ms (71.74%), Space: 15.1 MB (11.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,16 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> seen;
+        std::unordered_map<int, size_t> seen;
 
-        for (size_t i = 0; i < nums.size(); ++i)
-        {
+        for (size_t i = 0; i < nums.size(); i++) {
             auto complement = target - nums[i];
 
-            if (auto it = seen.find(complement); it != seen.end())
-            {
-                return { it->second, static_cast<int>(i) };
+            if (auto it = seen.find(complement); it != seen.end()) {
+                return { static_cast<int>(it->second), static_cast<int>(i) };
             }
 
             seen[nums[i]] = i;
